1e1b8649dcaf572712414c5626598d39cd3b07bc
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-10-18 21:22:16 +0300

    Add project skeleton

---
+++ src/main/java/com/omarahmed42/main/Main.java
@@ -0,0 +1,14 @@
+package com.omarahmed42.main;
+
+import com.omarahmed42.Window;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
