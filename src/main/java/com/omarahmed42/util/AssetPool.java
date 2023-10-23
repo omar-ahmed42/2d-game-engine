14e12cbb818128283f273df4ae6fb901f13c9bc3
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-10-23 15:01:18 +0300

    Move sprite related logic to sprite class and add spritesheets

--- src/main/java/com/omarahmed42/util/AssetPool.java
+++ src/main/java/com/omarahmed42/util/AssetPool.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.omarahmed42.components.Spritesheet;
 import com.omarahmed42.renderer.Shader;
 import com.omarahmed42.renderer.Texture;
 
 public class AssetPool {
     private static Map<String, Shader> shaders = new HashMap<>();
     private static Map<String, Texture> textures = new HashMap<>();
+    private static Map<String, Spritesheet> spritesheets = new HashMap<>();
 
     public static Shader getShader(String resourceName) {
         File file = new File(resourceName);
@@ -33,5 +35,22 @@ public class AssetPool {
             AssetPool.textures.put(file.getAbsolutePath(), texture);
             return texture;
         }
+    }
+
+    public static void addSpritesheet(String resourceName, Spritesheet spritesheet) {
+        File file = new File(resourceName);
+        if (!AssetPool.spritesheets.containsKey(file.getAbsolutePath())) {
+            AssetPool.spritesheets.put(file.getAbsolutePath(), spritesheet);
+        }
+    }
+
+    public static Spritesheet getSpritesheet(String resourceName) {
+        File file = new File(resourceName);
+        if (!AssetPool.spritesheets.containsKey(file.getAbsolutePath())) {
+            assert false : "Error: Tried to access spritesheet '" + resourceName
+                    + "' and it has not been added to assets pool";
+        }
+
+        return AssetPool.spritesheets.getOrDefault(file.getAbsolutePath(), null);
     }
 }
