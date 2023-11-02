caf7425b22beaadde10db1289291005d491d63fc
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-11-02 16:54:13 +0200

    Fix a bug that caused duplicated objects to not appear

--- src/main/java/com/omarahmed42/components/KeyControls.java
+++ src/main/java/com/omarahmed42/components/KeyControls.java
@@ -18,21 +18,27 @@ public class KeyControls extends Component {
         PropertiesWindow propertiesWindow = Window.getImGuiLayer().getPropertiesWindow();
         GameObject activeGameObject = propertiesWindow.getActiveGameObject();
         List<GameObject> activeGameObjects = propertiesWindow.getActiveGameObjects();
-        
 
-        if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_CONTROL) && KeyListener.keyBeginPress(GLFW_KEY_D) && activeGameObject != null) {
+        if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_CONTROL) && KeyListener.keyBeginPress(GLFW_KEY_D)
+                && activeGameObject != null) {
             GameObject newObj = activeGameObject.copy();
             Window.getScene().addGameObjectToScene(newObj);
             newObj.transform.position.add(Settings.GRID_WIDTH, 0.0f);
             propertiesWindow.setActiveGameObject(newObj);
-        } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_CONTROL) && 
-        KeyListener.keyBeginPress(GLFW_KEY_D) && activeGameObjects.size() > 1) {
+            if (newObj.getComponent(StateMachine.class) != null) {
+                newObj.getComponent(StateMachine.class).refreshTextures();
+            }
+        } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_CONTROL) &&
+                KeyListener.keyBeginPress(GLFW_KEY_D) && activeGameObjects.size() > 1) {
             List<GameObject> gameObjects = new ArrayList<>(activeGameObjects);
             propertiesWindow.clearSelected();
             for (GameObject go : gameObjects) {
                 GameObject copy = go.copy();
                 Window.getScene().addGameObjectToScene(copy);
                 propertiesWindow.addActiveGameObject(copy);
+                if (copy.getComponent(StateMachine.class) != null) {
+                    copy.getComponent(StateMachine.class).refreshTextures();
+                }
             }
 
         } else if (KeyListener.keyBeginPress(GLFW_KEY_DELETE)) {
